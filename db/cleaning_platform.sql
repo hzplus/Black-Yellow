bc6a1b6999f9cf7fc0b2cf8c9161c546b3fd88d2
hzplus <hongzhetee@gmail.com>
2025-05-09 18:58:08 +0800

    Update cleaning_platform.sql
    added the fking thing the fking confirmed_matcehs table

--- db/cleaning_platform.sql
+++ db/cleaning_platform.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 08, 2025 at 08:39 AM
+-- Generation Time: May 09, 2025 at 12:56 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,16 +24,23 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `match_history`
+-- Table structure for table `confirmed_matches`
 --
 
-CREATE TABLE `match_history` (
+CREATE TABLE `confirmed_matches` (
   `matchid` int(11) NOT NULL,
-  `cleanerid` int(11) DEFAULT NULL,
-  `homeownerid` int(11) DEFAULT NULL,
-  `serviceid` int(11) DEFAULT NULL,
-  `match_date` date DEFAULT NULL
+  `cleanerid` int(11) NOT NULL,
+  `homeownerid` int(11) NOT NULL,
+  `serviceid` int(11) NOT NULL,
+  `confirmed_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `confirmed_matches`
+--
+
+INSERT INTO `confirmed_matches` (`matchid`, `cleanerid`, `homeownerid`, `serviceid`, `confirmed_at`) VALUES
+(1, 6, 9, 9, '2025-05-09 18:42:26');
 
 -- --------------------------------------------------------
 
@@ -61,7 +68,9 @@ CREATE TABLE `services` (
 INSERT INTO `services` (`serviceid`, `cleanerid`, `title`, `description`, `price`, `category`, `view_count`, `shortlist_count`, `image_path`, `availability`) VALUES
 (2, 6, 'waxonwaxoff', 'waxonwaxoff', 1000.00, 'Floor', 0, 0, NULL, 'Mon-Fri 9AM-12PM'),
 (8, 6, 'ss', 'ss', 111.00, 'All-in-one', 0, 0, NULL, 'Mon-Fri 9AM-12PM'),
-(9, 6, 'Wax on Wax off', 'a', 1000.00, 'Floor', 0, 0, 'assets/images/68176e27f3b10_waxonwaxoff.png', 'Mon-Fri 9AM-12PM');
+(9, 6, 'Wax on Wax off', 'a', 1000.00, 'Floor', 0, 0, 'assets/images/68176e27f3b10_waxonwaxoff.png', 'Mon-Fri 9AM-12PM'),
+(10, 6, 'adada', 'aaaadaa', 1.00, 'Floor', 0, 0, NULL, 'Flexible'),
+(13, 6, 'bbblol', 'bbbhehe', 2.00, 'Laundry', 0, 0, 'assets/images/681da7e0009fb_waxonwaxoff.png', 'Mon-Fri 9AM-12PM');
 
 -- --------------------------------------------------------
 
@@ -123,16 +132,18 @@ INSERT INTO `users` (`userid`, `username`, `email`, `password`, `role`, `status`
 (4, 'admin3', 'admin3@email.com', '$2y$10$QzhAfTVkSAdUPI5ENcmXwu2mZHFMfGfxnWoe.01rBcV.Lyq8da9T.', 'Admin', 'active'),
 (5, 'admin4', 'admin4@email.com', '$2y$10$sOCJjWf68vHZjNkWuBWtp.PiNB3f/FDE1mXf92fsoVNqjlqvBnJUC', 'Admin', 'active'),
 (6, 'cleaner', 'cleaner@gmail.com', '$2y$10$/zfv1vdwwyBbrZEcc6Es1e7t6WESTh.13.Vo6prb5lAoytNHa7ds2', 'Cleaner', 'active'),
-(7, 'cleaner2', 'cleaner2@gmail.com', '$2y$10$Q6tPaFl17jCM3We/xxnrduEw8P1quLaEq1kiN5/up0shSLCO.iFle', 'Cleaner', 'active');
+(7, 'cleaner2', 'cleaner2@gmail.com', '$2y$10$Q6tPaFl17jCM3We/xxnrduEw8P1quLaEq1kiN5/up0shSLCO.iFle', 'Cleaner', 'active'),
+(8, 'cleanernum2', 'cleanernum2@gmail.com', '$2y$10$E4tdpld0jxuxqeevLDPUquzXPoWLta2nfPhxnlwWu89kGapAFdwje', 'Cleaner', 'active'),
+(9, 'homeowner', 'homeowner@gmail.com', '$2y$10$9LA0ymxSeN4lsM0c2n2oDebnoALGA0c83a9OkhCDfxliodmY0r74u', 'Homeowner', 'active');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `match_history`
+-- Indexes for table `confirmed_matches`
 --
-ALTER TABLE `match_history`
+ALTER TABLE `confirmed_matches`
   ADD PRIMARY KEY (`matchid`),
   ADD KEY `cleanerid` (`cleanerid`),
   ADD KEY `homeownerid` (`homeownerid`),
@@ -171,16 +182,16 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `match_history`
+-- AUTO_INCREMENT for table `confirmed_matches`
 --
-ALTER TABLE `match_history`
-  MODIFY `matchid` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `confirmed_matches`
+  MODIFY `matchid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `services`
 --
 ALTER TABLE `services`
-  MODIFY `serviceid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `serviceid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `service_categories`
@@ -198,19 +209,19 @@ ALTER TABLE `shortlists`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `match_history`
+-- Constraints for table `confirmed_matches`
 --
-ALTER TABLE `match_history`
-  ADD CONSTRAINT `match_history_ibfk_1` FOREIGN KEY (`cleanerid`) REFERENCES `users` (`userid`),
-  ADD CONSTRAINT `match_history_ibfk_2` FOREIGN KEY (`homeownerid`) REFERENCES `users` (`userid`),
-  ADD CONSTRAINT `match_history_ibfk_3` FOREIGN KEY (`serviceid`) REFERENCES `services` (`serviceid`);
+ALTER TABLE `confirmed_matches`
+  ADD CONSTRAINT `confirmed_matches_ibfk_1` FOREIGN KEY (`cleanerid`) REFERENCES `users` (`userid`),
+  ADD CONSTRAINT `confirmed_matches_ibfk_2` FOREIGN KEY (`homeownerid`) REFERENCES `users` (`userid`),
+  ADD CONSTRAINT `confirmed_matches_ibfk_3` FOREIGN KEY (`serviceid`) REFERENCES `services` (`serviceid`);
 
 --
 -- Constraints for table `services`
