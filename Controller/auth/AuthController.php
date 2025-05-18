b6d98b2c5a83bb3406c2911747f1f92204f10cef
ianchoy <joonian656@gmail.com>
2025-05-18 17:04:00 +0800

    Update AuthController.php

--- Controller/auth/AuthController.php
+++ Controller/auth/AuthController.php
@@ -4,7 +4,11 @@
 require_once __DIR__ . '/../../Entity/User.php';
 
 class authController {
-    public function login($username, $password, $role) {
-        return User::login($username, $password, $role);
+    public function getUser($username, $role) {
+        return User::findByUsernameAndRole($username, $role);
     }
+    
+    public function getRoleStatus($role) {
+    return User::getRoleStatus($role);
+}
 }
